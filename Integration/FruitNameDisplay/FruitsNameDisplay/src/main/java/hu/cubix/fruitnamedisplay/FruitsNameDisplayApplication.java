d7c807f1969115ab4cfab6d2ae5ba24ab05463d5
GboHalasz <hal2297dev@gmail.com>
2024-04-10 15:34:22 +0200

    FruitNameDisplay java app initialization.

---
+++ Integration/FruitNameDisplay/FruitsNameDisplay/src/main/java/hu/cubix/fruitnamedisplay/FruitsNameDisplayApplication.java
@@ -0,0 +1,13 @@
+package hu.cubix.fruitnamedisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsNameDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsNameDisplayApplication.class, args);
+	}
+
+}
---
+++ Integration/FruitNameDisplay/FruitsNameDisplay/src/test/java/hu/cubix/fruitnamedisplay/FruitsNameDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cubix.fruitnamedisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsNameDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
