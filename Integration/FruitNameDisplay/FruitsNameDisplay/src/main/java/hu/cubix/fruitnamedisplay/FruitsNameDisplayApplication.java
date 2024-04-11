2a4878e66f5196840133da0e8d4b2fcf3c10d886
GboHalasz <hal2297dev@gmail.com>
2024-04-11 12:01:04 +0200

    "RequestController required a bean of type that could not be found" error correction.

--- Integration/FruitNameDisplay/FruitsNameDisplay/src/main/java/hu/cubix/fruitnamedisplay/FruitsNameDisplayApplication.java
+++ Integration/FruitNameDisplay/FruitsNameDisplay/src/main/java/hu/cubix/fruitnamedisplay/FruitsNameDisplayApplication.java
@@ -2,8 +2,10 @@ package hu.cubix.fruitnamedisplay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan ({"hu.cubix.fruitnamedisplay"})
 public class FruitsNameDisplayApplication {
 
 	public static void main(String[] args) {
