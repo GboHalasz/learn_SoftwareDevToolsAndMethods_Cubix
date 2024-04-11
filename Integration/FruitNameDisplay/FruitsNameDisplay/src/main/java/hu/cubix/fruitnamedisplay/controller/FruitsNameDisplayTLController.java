a478f9dfc0cce86ce608f0cb90c64bb37a450445
GboHalasz <hal2297dev@gmail.com>
2024-04-11 11:40:25 +0200

    Refactor TL controller and remove comment from pom.xml.

--- Integration/FruitNameDisplay/FruitsNameDisplay/src/main/java/hu/cubix/fruitnamedisplay/controller/FruitsNameDisplayTLController.java
+++ Integration/FruitNameDisplay/FruitsNameDisplay/src/main/java/hu/cubix/fruitnamedisplay/controller/FruitsNameDisplayTLController.java
@@ -5,16 +5,18 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import hu.cubix.fruitnamedisplay.service.FruitNamesService;
 
 @Controller
+@RequestMapping("/")
 public class FruitsNameDisplayTLController {
 
 	@Autowired
 	FruitNamesService fruitNamesService;
 	
-	@GetMapping("/")
+	@GetMapping
 	public String home(Map<String, Object> model) {
 		model.put("fruits", fruitNamesService.getFruits());
 		return "index";
